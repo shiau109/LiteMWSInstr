b5fb2dc41a753ad5730250ea174ca7bbe766204e
RatisWu <61738702+RatisWu@users.noreply.github.com>
2025-08-04 11:03:23 +0800

    make it package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='liteInstru',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['numpy','matplotlib','pyvisa','qcodes','scipy'],
+    author='shiau109, RatisWu',
+    author_email='porkface0301@gmail.com',
+    description='Extensions for LiteVNA',
+    url='https://github.com/shiau109/LiteVNA',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
